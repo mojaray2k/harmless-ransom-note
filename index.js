a3d8312062b8822672c34f98842098567119ce2a
Amen Moja Ra <mojaray2k@gmail.com>
2021-07-11 15:43:44 +0000

    harmlessRansomNote part 1

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function harmlessRansomNote(noteText, magazineText){
+  var noteArr = noteText.split(' ');
+  var magazineArr = magazineText.split(' ');
+  var magazineObj = {};
 
+  magazineArr.forEach(word =>  {
+    if(!magazineObj[word]) magazineObj[word] = 0;
+    magazineObj[word]++;
+  });
+
+  console.log(magazineObj);
 }
+
+harmlessRansomNote('', 'This is all the magazine text in the magazine');
