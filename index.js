423376ce1a9c2c583122cefdc991748c45c67d63
Amen Moja Ra <mojaray2k@gmail.com>
2021-07-11 03:23:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function harmlessRansomNote(noteText, magazineText){
+
+}
+
+// Constant runtime
+// Big ) Notation: "0 (1)"
+function log(array) {
+  console.log(array[0]);
+  console.log(array[1]);
+}
+log([1,2,3,4]);
+log([1,2,3,4,5,6,7,8,9,10]);
